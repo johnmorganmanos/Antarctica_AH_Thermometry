0f259cac80ee4f740cbaee9654115b7467fdaeda
John-Morgan Manos <jmanos@sermeq.ess.washington.edu>
2024-06-06 12:23:48 -0700

    updated heat package with variable accumulation rates and added notebook Untitled to visualize.

--- heat.py
+++ heat.py
@@ -56,6 +56,64 @@ def heat(t_surf,
 
     return U,t,z
 
+def heat_var_accum(t_surf, 
+                   accumulation,
+         tmax = 1000,
+         tmin = 0,
+         zmax = 100,
+         dTdz = 0.02,
+         nz = 39,
+         nt = 99,
+         alpha = 35,
+         S = np.nan 
+        ):
+    
+    '''
+    Solves the advection/diffusion equation with mixed temperature/heat flux boundary conditions
+    '''
+    
+    if np.isnan(S).any():
+        S = np.zeros((nz+1,nt+1))
+        
+    z0=0    
+    dz = zmax/(nz+1)
+    dt = (tmax-tmin)/nt
+    t = np.linspace(tmin,tmax, nt+1)
+    z = np.linspace(dz,zmax, nz+1)
+
+    cfl = alpha*dt/(dz**2)
+    Azz = np.diag([1+2*cfl] * (nz+1)) + np.diag([-cfl] * (nz),k=1)\
+        + np.diag([-cfl] * (nz),k=-1)
+
+
+
+    # Initial condition: gradient equal to basal gradient and equal to surface temp.
+    U = np.zeros((nz+1,nt+1))
+    U[:,0] = t_surf[0] + z*dTdz
+
+    for k in range(nt):
+
+        
+        w = - accumulation[k] * np.ones(nz)
+        abc = w*dt/(2*dz)
+        Az = np.diag(abc,k=1) - np.diag(abc,k=-1)
+        Az[0,:] =0
+        Az[-1,:]=0
+        A = Azz - Az
+
+
+        # Neumann boundary condition
+        A[nz,nz-1] = -2*cfl
+        b= np.zeros((nz+1,1))
+        b[nz] =  2*cfl*dz * dTdz
+        b[0] = cfl*t_surf[k]    #  Dirichlet boundary condition
+
+        c = U[:,k] + b.flatten() + S[:,k+1]*dt # previous values + dirichlet + sources
+        U[:,k+1] = np.linalg.solve(A,c)
+
+    return U,t,z
+
+
 def heat_plot(t,t_surf,start_year,end_year,z,U,plot_start_frac=0.95):
     nt=len(t)
     plt.subplots(2,2,figsize=(8,9))
